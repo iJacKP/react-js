7d09d28e4ed977a3ddca1ed5f93e5c9f0bd9cfee
Jack <jackson_d2@outlook.com>
2021-05-05 17:13:29 -0300

    Doing some challenges just to get usual with props

---
+++ react-props-practice/src/components/Detail.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Detail(props){
+    return(
+        <div>
+        <p className="info">{props.infoDetails}</p>
+        </div>
+      )
+}
+
+export default Detail
